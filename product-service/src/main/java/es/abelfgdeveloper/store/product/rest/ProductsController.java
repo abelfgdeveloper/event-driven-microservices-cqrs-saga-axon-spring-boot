00af5bda97227b2cc3611bab1ecc2b6cbcaeb203
Abel Fernandez <abelfgdeveloper@gmail.com>
2022-04-30 00:44:53 +0200

    Section 3 - Creating a Basic Microservice - Class 4 - Handle HTTP POST, GET, PUT and DELETE Requests

--- product-service/src/main/java/es/abelfgdeveloper/store/product/rest/ProductsController.java
+++ product-service/src/main/java/es/abelfgdeveloper/store/product/rest/ProductsController.java
@@ -1,8 +1,33 @@
 package es.abelfgdeveloper.store.product.rest;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
-public class ProductsController {}
+public class ProductsController {
+
+  @PostMapping
+  public String createProduct() {
+    return "HTTP POST Handled";
+  }
+  
+  @GetMapping
+  public String getProduct() {
+    return "HTTP GET Handled";
+  }
+  
+  @PutMapping
+  public String updateProduct() {
+    return "HTTP PUT Handled";
+  }
+  
+  @DeleteMapping
+  public String deleteProduct() {
+    return "HTTP DELETE Handled";
+  }
+}
