20eaf3dd5fcffc2ff189df5eccd4b16c41c5acf6
Abel Fernandez <abelfgdeveloper@gmail.com>
2022-04-30 15:33:57 +0200

    Section 5 - Spring Cloud API Gateway & Load Balancing - Class 6 - Trying how load balancing work

--- product-service/src/main/java/es/abelfgdeveloper/store/product/rest/ProductsController.java
+++ product-service/src/main/java/es/abelfgdeveloper/store/product/rest/ProductsController.java
@@ -1,5 +1,7 @@
 package es.abelfgdeveloper.store.product.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products")
 public class ProductsController {
 
+  @Autowired private Environment env;
+
   @PostMapping
   public String createProduct() {
     return "HTTP POST Handled";
@@ -18,7 +22,7 @@ public class ProductsController {
 
   @GetMapping
   public String getProduct() {
-    return "HTTP GET Handled";
+    return "HTTP GET Handled" + env.getProperty("local.server.port");
   }
 
   @PutMapping
