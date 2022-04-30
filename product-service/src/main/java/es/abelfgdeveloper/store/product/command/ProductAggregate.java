11030414adec8e753fb4aa11ada025319a1e27c8
Abel Fernandez <abelfgdeveloper@gmail.com>
2022-04-30 16:43:02 +0200

    Section 7 - CQRS Pattern. Products Microservice - Class 10 - Validate the CreateProductCommand

--- product-service/src/main/java/es/abelfgdeveloper/store/product/command/ProductAggregate.java
+++ product-service/src/main/java/es/abelfgdeveloper/store/product/command/ProductAggregate.java
@@ -1,5 +1,6 @@
 package es.abelfgdeveloper.store.product.command;
 
+import java.math.BigDecimal;
 import lombok.NoArgsConstructor;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.spring.stereotype.Aggregate;
@@ -11,6 +12,12 @@ public class ProductAggregate {
   @CommandHandler
   public ProductAggregate(CreateProductCommand command) {
     // Validate Create Product Command
+    if (command.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
+      throw new IllegalArgumentException("Price cannot be less or equal than zero");
+    }
 
+    if (command.getTitle() == null || command.getTitle().isBlank()) {
+      throw new IllegalArgumentException("Title cannot be empty");
+    }
   }
 }
