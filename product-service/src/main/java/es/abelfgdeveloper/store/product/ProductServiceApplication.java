9f891439eae1c5096cb5048bcdab2ecd93d4274d
Abel Fernandez <abelfgdeveloper@gmail.com>
2022-04-30 18:54:26 +0200

    Section 12 - Validation. Message Dispatch Interceptor - Class 3 - Register Message Dispatch Interceptor

--- product-service/src/main/java/es/abelfgdeveloper/store/product/ProductServiceApplication.java
+++ product-service/src/main/java/es/abelfgdeveloper/store/product/ProductServiceApplication.java
@@ -1,8 +1,12 @@
 package es.abelfgdeveloper.store.product;
 
+import es.abelfgdeveloper.store.product.command.interceptor.CreateProductCommandInterceptor;
+import org.axonframework.commandhandling.CommandBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,5 +14,11 @@ public class ProductServiceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ProductServiceApplication.class, args);
+  }
+
+  @Autowired
+  public void registerCreateProductCommandInterceptor(
+      ApplicationContext context, CommandBus commandBus) {
+    commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
   }
 }
