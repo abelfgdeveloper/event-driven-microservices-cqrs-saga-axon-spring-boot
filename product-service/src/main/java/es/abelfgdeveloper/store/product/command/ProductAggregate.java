ad3cfe4e809a5f1f155488a627e186e5732ac400
Abel Fernandez <abelfgdeveloper@gmail.com>
2022-04-30 16:52:17 +0200

    Section 7 - CQRS Pattern. Products Microservice - Class 13 - @EventSourcingHandler

--- product-service/src/main/java/es/abelfgdeveloper/store/product/command/ProductAggregate.java
+++ product-service/src/main/java/es/abelfgdeveloper/store/product/command/ProductAggregate.java
@@ -4,12 +4,19 @@ import es.abelfgdeveloper.store.product.core.event.ProductCreatedEvent;
 import java.math.BigDecimal;
 import lombok.NoArgsConstructor;
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
 
 @NoArgsConstructor
 @Aggregate
 public class ProductAggregate {
+
+  @AggregateIdentifier private String productId;
+  private String title;
+  private BigDecimal price;
+  private Integer quantity;
 
   @CommandHandler
   public ProductAggregate(CreateProductCommand command) {
@@ -29,5 +36,13 @@ public class ProductAggregate {
     createdEvent.setQuantity(command.getQuantity());
 
     AggregateLifecycle.apply(createdEvent);
+  }
+
+  @EventSourcingHandler
+  public void on(ProductCreatedEvent event) {
+    this.productId = event.getProductId();
+    this.title = event.getTitle();
+    this.price = event.getPrice();
+    this.quantity = event.getQuantity();
   }
 }
