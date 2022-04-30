4c37b963d9f63fe73feb1d750398d389dc972cca
Abel Fernandez <abelfgdeveloper@gmail.com>
2022-04-30 17:38:35 +0200

    Section 8 - CQRS. Persisting Event in the Products database - Class 4 - Creating an ProductEntity

---
+++ product-service/src/main/java/es/abelfgdeveloper/store/product/core/data/ProductEntity.java
@@ -0,0 +1,27 @@
+package es.abelfgdeveloper.store.product.core.data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "products")
+@Data
+public class ProductEntity implements Serializable {
+
+  private static final long serialVersionUID = -4999527428495950474L;
+
+  @Id
+  @Column(unique = true)
+  private String productId;
+
+  @Column(unique = true)
+  private String title;
+
+  private BigDecimal price;
+  private Integer quantity;
+}
