aa05798bdad527e138d6cae4ee4355af344d6a29
Abel Fernandez <abelfgdeveloper@gmail.com>
2022-04-30 18:11:35 +0200

    Section 9 - CQRS. Querying Data - Class 8 - Trying how it works

--- product-service/src/main/java/es/abelfgdeveloper/store/product/command/rest/ProductCommandController.java
+++ product-service/src/main/java/es/abelfgdeveloper/store/product/command/rest/ProductCommandController.java
@@ -5,10 +5,7 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,18 +38,18 @@ public class ProductCommandController {
     return returnValue;
   }
 
-  @GetMapping
-  public String getProduct() {
-    return "HTTP GET Handled" + env.getProperty("local.server.port");
-  }
-
-  @PutMapping
-  public String updateProduct() {
-    return "HTTP PUT Handled";
-  }
-
-  @DeleteMapping
-  public String deleteProduct() {
-    return "HTTP DELETE Handled";
-  }
+  //  @GetMapping
+  //  public String getProduct() {
+  //    return "HTTP GET Handled" + env.getProperty("local.server.port");
+  //  }
+  //
+  //  @PutMapping
+  //  public String updateProduct() {
+  //    return "HTTP PUT Handled";
+  //  }
+  //
+  //  @DeleteMapping
+  //  public String deleteProduct() {
+  //    return "HTTP DELETE Handled";
+  //  }
 }
