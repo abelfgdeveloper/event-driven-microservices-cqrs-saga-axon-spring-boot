ecfb8b565ae5333f0b9ad57dad50d35d746f6a82
Abel Fernandez <abelfgdeveloper@gmail.com>
2022-04-30 17:53:29 +0200

    Section 9 - CQRS. Querying Data - Class 2 - Creating Rest Controller

---
+++ product-service/src/main/java/es/abelfgdeveloper/store/product/query/rest/ProductQueryController.java
@@ -0,0 +1,8 @@
+package es.abelfgdeveloper.store.product.query.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductQueryController {}
