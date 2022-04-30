a2de8ed81573b0f23d8b75ddb3cee70de9ee8f9c
Abel Fernandez <abelfgdeveloper@gmail.com>
2022-04-30 17:45:56 +0200

    Section 8 - CQRS. Persisting Event in the Products database - Class 7 - Implementing @EventHandler method

--- product-service/src/main/java/es/abelfgdeveloper/store/product/query/ProductEventHandler.java
+++ product-service/src/main/java/es/abelfgdeveloper/store/product/query/ProductEventHandler.java
@@ -1,12 +1,26 @@
 package es.abelfgdeveloper.store.product.query;
 
+import es.abelfgdeveloper.store.product.core.data.ProductEntity;
+import es.abelfgdeveloper.store.product.core.data.ProductRepository;
 import es.abelfgdeveloper.store.product.core.event.ProductCreatedEvent;
+import lombok.RequiredArgsConstructor;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class ProductEventHandler {
 
+  private final ProductRepository productRepository;
+
   @EventHandler
-  public void on(ProductCreatedEvent event) {}
+  public void on(ProductCreatedEvent event) {
+    ProductEntity productEntity = new ProductEntity();
+    productEntity.setProductId(event.getProductId());
+    productEntity.setTitle(event.getTitle());
+    productEntity.setPrice(event.getPrice());
+    productEntity.setQuantity(event.getQuantity());
+
+    productRepository.save(productEntity);
+  }
 }
