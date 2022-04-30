95fcaf8401c81c420954fdef87ba30a29ec61549
Abel Fernandez <abelfgdeveloper@gmail.com>
2022-04-30 14:11:54 +0200

    Section 4 - Eureka Discovery - Class 2 - Creating new project

--- product-service/src/main/java/es/abelfgdeveloper/store/product/rest/ProductsController.java
+++ product-service/src/main/java/es/abelfgdeveloper/store/product/rest/ProductsController.java
@@ -15,17 +15,17 @@ public class ProductsController {
   public String createProduct() {
     return "HTTP POST Handled";
   }
-  
+
   @GetMapping
   public String getProduct() {
     return "HTTP GET Handled";
   }
-  
+
   @PutMapping
   public String updateProduct() {
     return "HTTP PUT Handled";
   }
-  
+
   @DeleteMapping
   public String deleteProduct() {
     return "HTTP DELETE Handled";
