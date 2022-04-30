75aa3464fffb149625cbd31e27ad2fc785b48959
Abel Fernandez <abelfgdeveloper@gmail.com>
2022-04-30 16:45:39 +0200

    Section 7 - CQRS Pattern. Products Microservice - Class 11 - Creating ProductCreatedEvent

---
+++ product-service/src/main/java/es/abelfgdeveloper/store/product/core/event/ProductCreatedEvent.java
@@ -0,0 +1,13 @@
+package es.abelfgdeveloper.store.product.core.event;
+
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Data
+public class ProductCreatedEvent {
+
+  private String productId;
+  private String title;
+  private BigDecimal price;
+  private Integer quantity;
+}
