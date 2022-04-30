afc72482491389cf5b426df358ff4d069742adc5
Abel Fernandez <abelfgdeveloper@gmail.com>
2022-04-30 18:09:24 +0200

    Section 9 - CQRS. Querying Data - Class 7 - Implementing the findProducts() method

--- product-service/src/main/java/es/abelfgdeveloper/store/product/query/ProductQueryHandler.java
+++ product-service/src/main/java/es/abelfgdeveloper/store/product/query/ProductQueryHandler.java
@@ -1,7 +1,12 @@
 package es.abelfgdeveloper.store.product.query;
 
+import es.abelfgdeveloper.store.product.core.data.ProductEntity;
 import es.abelfgdeveloper.store.product.core.data.ProductRepository;
+import es.abelfgdeveloper.store.product.query.rest.ProductRestModel;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
@@ -9,4 +14,22 @@ import org.springframework.stereotype.Component;
 public class ProductQueryHandler {
 
   private final ProductRepository productRepository;
+
+  @QueryHandler
+  public List<ProductRestModel> findProducts(FindProductsQuery query) {
+    List<ProductRestModel> productsRest = new ArrayList<>();
+
+    List<ProductEntity> storedProducts = productRepository.findAll();
+
+    for (ProductEntity productEntity : storedProducts) {
+      ProductRestModel productRestModel = new ProductRestModel();
+      productRestModel.setProductId(productEntity.getProductId());
+      productRestModel.setTitle(productEntity.getTitle());
+      productRestModel.setPrice(productEntity.getPrice());
+      productRestModel.setQuantity(productEntity.getQuantity());
+      productsRest.add(productRestModel);
+    }
+
+    return productsRest;
+  }
 }
