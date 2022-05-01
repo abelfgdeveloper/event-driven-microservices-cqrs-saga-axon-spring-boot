3c1255b48b1c8acfdfab6b230f6cab8044662771
Abel Fernandez <abelfgdeveloper@gmail.com>
2022-05-01 16:10:57 +0200

    Section 14 - Handle Error & Rollback Transaction - Class 9 - Register the ListenerInvocationErrorHandler

--- product-service/src/main/java/es/abelfgdeveloper/store/product/ProductServiceApplication.java
+++ product-service/src/main/java/es/abelfgdeveloper/store/product/ProductServiceApplication.java
@@ -1,7 +1,9 @@
 package es.abelfgdeveloper.store.product;
 
 import es.abelfgdeveloper.store.product.command.interceptor.CreateProductCommandInterceptor;
+import es.abelfgdeveloper.store.product.core.errorhandling.ProductServiceEventErrorHandler;
 import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.config.EventProcessingConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,5 +22,14 @@ public class ProductServiceApplication {
   public void registerCreateProductCommandInterceptor(
       ApplicationContext context, CommandBus commandBus) {
     commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
+  }
+
+  @Autowired
+  public void configure(EventProcessingConfigurer config) {
+    config.registerListenerInvocationErrorHandler(
+        "product-group", conf -> new ProductServiceEventErrorHandler());
+
+    //    config.registerListenerInvocationErrorHandler("product-group", conf ->
+    // PropagatingErrorHandler.instance());
   }
 }
